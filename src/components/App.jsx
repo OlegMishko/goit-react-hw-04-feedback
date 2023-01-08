4a74a276c16414e3c53d105479875a0fa2b4a869
OlegMishko <oleg.mishko@gmail.com>
2023-01-08 22:49:29 +0200

    Update App.jsx
    Добавлено Хук useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { Notifications } from './Notifications/Notifications';
 import { MainPage } from 'common/MainPage.styled';
@@ -6,60 +6,112 @@ import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
-    // const values = Object.values(this.state);
-    // let total = 0;
-    // return values.map(item => (total += item), total).reverse()[0];
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  handleClick = event => {
-    const { value } = event.target;
-    this.setState(prevState => ({
-      [value]: prevState[value] + 1,
-    }));
+  const handleClick = event => {
+    const feedback = event.target.textContent;
+    switch (feedback) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  const options = Object.keys({ good, neutral, bad });
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-    return (
-      <MainPage>
-        <Section title="Please leave feadback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleClick}
+  return (
+    <MainPage>
+      <Section title="Please leave feadback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+      </Section>
+      <Section title="Statistics:">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics:">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notifications message="There is no feedback"></Notifications>
-          )}
-        </Section>
-      </MainPage>
-    );
-  }
-}
+        ) : (
+          <Notifications message="There is no feedback"></Notifications>
+        )}
+      </Section>
+    </MainPage>
+  );
+};
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   };
+
+//   handleClick = event => {
+//     const { value } = event.target;
+//     this.setState(prevState => ({
+//       [value]: prevState[value] + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const options = Object.keys(this.state);
+//     const total = this.countTotalFeedback();
+//     const positivePercentage = this.countPositiveFeedbackPercentage();
+
+//     return (
+//       <MainPage>
+//         <Section title="Please leave feadback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.handleClick}
+//           />
+//         </Section>
+//         <Section title="Statistics:">
+//           {total > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           ) : (
+//             <Notifications message="There is no feedback"></Notifications>
+//           )}
+//         </Section>
+//       </MainPage>
+//     );
+//   }
